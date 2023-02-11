1a72f52f74ae1a642db8cedd1f300afa0789365f
@AmirAK777 <contactezpro@gmail.com>
2023-02-11 11:51:47 +0100

    Test @PostMapping / addBox

--- src/main/kotlin/com/example/sushishop/controller/BoxController.kt
+++ src/main/kotlin/com/example/sushishop/controller/BoxController.kt
@@ -1,5 +1,7 @@
 package com.example.sushishop.controller
+import com.example.sushishop.model.Box
 import com.example.sushishop.repository.BoxRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -7,5 +9,11 @@ import java.util.*
 @RestController
 @RequestMapping("/boxes")
 class BoxController(val boxRepository: BoxRepository) {
+
+    @PostMapping
+    fun addBox(@RequestBody box: Box): ResponseEntity<Box> {
+        val newBox = boxRepository.save(box)
+        return ResponseEntity.ok(newBox)
+    }
 
 }
